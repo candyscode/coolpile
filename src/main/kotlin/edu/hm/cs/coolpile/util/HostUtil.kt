4ab58e673d2f33e08e96a5f03f59ca3ab8b7c617
Andreas Thaler <andreas.thaler@quickbirdstudios.com>
2021-02-14 12:57:58 +0100

    revised runOnHost methods

--- src/main/kotlin/edu/hm/cs/coolpile/util/HostUtil.kt
+++ src/main/kotlin/edu/hm/cs/coolpile/util/HostUtil.kt
@@ -1,11 +1,24 @@
 package edu.hm.cs.coolpile.util
 
-fun runOnHost(command: String, onErrorAction: () -> Unit = {}) {
-    val returnCode = Runtime.getRuntime().exec(command).waitFor()
-    if (returnCode != 0) onErrorAction()
-}
+import java.io.BufferedReader
+import java.io.InputStreamReader
+import java.util.stream.Collectors
 
-fun runOnHost(command: Array<String>, onErrorAction: () -> Unit = {}) {
-    val returnCode = Runtime.getRuntime().exec(command).waitFor()
+fun runOnHost(command: String, onErrorAction: () -> Unit = {}) =
+        Runtime.getRuntime().exec(command).runOnHost(onErrorAction)
+
+fun runOnHost(command: Array<String>, onErrorAction: () -> Unit = {}) =
+        Runtime.getRuntime().exec(command).runOnHost(onErrorAction)
+
+private fun Process.runOnHost(onErrorAction: () -> Unit): String {
+    val returnCode = waitFor()
+
+    val stdError = BufferedReader(InputStreamReader(errorStream))
+
+    val errorString = stdError.lines().collect(Collectors.joining(","))
+    if (errorString.isNotEmpty()) println(errorString)
+
     if (returnCode != 0) onErrorAction()
+
+    return errorString
 }
